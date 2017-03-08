6f44d0260a26c419d45fd3f9ff5fbdf169319e2f
yzerlaut <yann.zerlaut@gmail.com>
2017-03-08 19:56:12 +0100

    update

--- time_freq.py
+++ time_freq.py
@@ -1,27 +1,31 @@
+import matplotlib.pylab as plt
+import numpy as np
 
-def plot(t, freqs, coefs):
+def time_freq_plot(t, freqs, data, coefs, xunits='s', yunits=''):
+    if xunits=='ms':
+        t = 1e3*t
     fig = plt.figure(figsize=(12,6))
     plt.subplots_adjust(wspace=.8, hspace=.5, bottom=.2)
     # signal plot
     plt.subplot2grid((3, 7), (0,0), colspan=6)
-    plt.plot(1e3*t, data)
-    plt.ylabel('signal');plt.xlabel('time (ms)')
-    plt.xlim([1e3*t[0], 1e3*t[-1]])
+    plt.plot(t, data)
+    plt.ylabel(yunits)
+    plt.xlim([t[0], t[-1]])
     # time frequency power plot
     plt.subplot2grid((3, 7), (1,0), rowspan=2, colspan=6)
-    c = plt.contourf(1e3*t, freqs, coefs, cmap='PRGn', aspect='auto')
-    plt.xlabel('time (ms)')
+    c = plt.contourf(t, freqs, coefs, cmap='PRGn', aspect='auto')
+    plt.xlabel('time ('+xunits+')')
     plt.ylabel('frequency (Hz)')
-    plt.yticks([10, 40, 70]);
     # mean power plot over intervals
     plt.subplot2grid((3, 7), (1, 6), rowspan=2)
     plt.xlabel('power')
     # max of power over intervals
     plt.subplot2grid((3, 8), (1, 7), rowspan=2)
-    plt.barh(freqs, np.power(coefs[:,cond],2).mean(axis=1),\
-             label='mean')
-    plt.plot(np.power(coefs[:,cond],2).mean(axis=1), freqs,\
-             label='max.')
+    plt.barh(freqs, np.power(coefs,2).mean(axis=1),\
+             label='mean', height=freqs[-1]-freqs[-2])
+    # plt.plot(np.power(coefs,2).max(axis=1), freqs,\
+    #          label='max.')
     plt.xlabel(' power')
     plt.legend(prop={'size':'small'}, loc=(0.1,1.1))
-    plt.show()
+    return fig
+
