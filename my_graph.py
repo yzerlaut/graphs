f73bcba32006f1e7f333db4545e1f0fd3fd6167a
yzerlaut <yann.zerlaut@gmail.com>
2017-07-06 18:24:13 +0200

    adding colors

--- my_graph.py
+++ my_graph.py
@@ -4,6 +4,11 @@ import matplotlib.pylab as plt
 import numpy as np
 mpl.rcParams.update({'axes.labelsize': 15, 'xtick.labelsize': 14, 'ytick.labelsize': 14})
 
+# custom colors
+Blue, Orange, Green, Red, Purple, Brown, Pink, Grey,\
+    Kaki, Cyan = '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728',\
+    '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf'
+
 def set_plot(ax, spines=['left', 'bottom'],\
                 num_xticks=5, num_yticks=5,\
                 xlabel='', ylabel='', tck_outward=5,\
