ab489c5079fed47220c6d9ac22454b1793da9dfd
yzerlaut <yann.zerlaut@gmail.com>
2018-03-15 18:36:28 +0100

    adding an automatic scaling function

--- annotations.py
+++ annotations.py
@@ -1,3 +1,6 @@
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),os.path.pardir))
+from graphs.scaling import FONTSIZE
 
 def from_pval_to_star(p,
                       threshold1=1e-3,
@@ -17,6 +20,27 @@ def sci_str(x, rounding=0, remove_0_in_exp=True):
     if remove_0_in_exp: y = y.replace('-0', '-')
     return y
 
+def bar_scales(ax,
+               Xbar=None, Ybar=None,
+               Xbar_label=None, Ybar_label=None,
+               location = 'top right',
+               factor=0.95):
+
+    x1, x2 = ax.get_xlim()
+    y1, y2 = ax.get_ylim()
+
+    if location=='top right':
+        x0 = x2-(1-factor)*(x2-x1)
+        y0 = y2-(1-factor)*(y2-y1)
+        
+    if Xbar is not None:
+        ax.plot([x0, x0-Xbar], [y0, y0], 'k-', lw=1)
+        ax.annotate(Xbar_label, (x0-Xbar, y0), fontsize=FONTSIZE)
+    if Ybar is not None:
+        ax.plot([x0, x0], [y0, y0-Ybar], 'k-', lw=1)
+        ax.annotate(Ybar_label, (x0, y0-Ybar), fontsize=FONTSIZE, rotation=90)
+
+    
 
 if __name__=='__main__':
     x = 32.23545345e-5
