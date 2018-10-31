32a957ae44f85975946d6ba3bc93867b18edd182
Yann Zerlaut <yann.zerlaut@gmail.com>
2018-10-31 18:11:14 +0100

    working version with movie for morpho comput

--- nrn_morpho.py
+++ nrn_morpho.py
@@ -28,15 +28,17 @@ def coordinate_projection(c, x0 ,y0, z0, polar_angle, azimuth_angle):
     z = c.z
     return x, y, z
 
-def get_segment_list(morpho, without_axon=False):
+def get_segment_list(morpho,
+                     polar_angle=0, azimuth_angle=np.pi/2., 
+                     without_axon=False):
 
     COMP_LIST = get_compartment_list(morpho, without_axon=without_axon)
 
     # the first one should be the 
     [x0, y0, z0] = COMP_LIST[0].x, COMP_LIST[0].y, COMP_LIST[0].z
-    xcoords = np.concatenate([coordinate_projection(c, x0 ,y0, z0, args.polar_angle, 0.)[0] for c in COMP_LIST])
-    ycoords = np.concatenate([coordinate_projection(c, x0 ,y0, z0, args.polar_angle, 0.)[1] for c in COMP_LIST])
-    zcoords = np.concatenate([coordinate_projection(c, x0 ,y0, z0, args.polar_angle, 0.)[2] for c in COMP_LIST])
+    xcoords = np.concatenate([coordinate_projection(c, x0 ,y0, z0, polar_angle, azimuth_angle)[0] for c in COMP_LIST])
+    ycoords = np.concatenate([coordinate_projection(c, x0 ,y0, z0, polar_angle, azimuth_angle)[1] for c in COMP_LIST])
+    zcoords = np.concatenate([coordinate_projection(c, x0 ,y0, z0, polar_angle, azimuth_angle)[2] for c in COMP_LIST])
     area = np.concatenate([c.area for c in COMP_LIST])
     comp_type = np.concatenate([[c.type for i in range(len(c.x))] for c in COMP_LIST])
 
@@ -67,7 +69,7 @@ def plot_nrn_shape(COMP_LIST,
         x, y, _ = coordinate_projection(c, x0 ,y0, z0, polar_angle, azimuth_angle)
         if (len(c.type.split('dend'))>1) or (len(c.type.split('apic'))>1):
             ax.plot(1e6*x, 1e6*y, '-', lw=lw, color=dend_color)
-        if (len(c.type.split('axon'))>1):
+        if (len(c.type.split('axon'))>1) and (axon_color!='None'):
             ax.plot(1e6*x, 1e6*y, '-', lw=lw, color=axon_color)
 
     # adding a bar for the spatial scale
@@ -91,49 +93,57 @@ def show_animated_time_varying_trace(t, Quant0, SEGMENT_LIST,
                                      polar_angle=0, azimuth_angle=np.pi/2.,
                                      quant_label='$V_m$ (mV)',
                                      time_label='time (ms)',
-                                     colormap=viridis_r, ms=2):
+                                     segment_condition=None,
+                                     colormap=viridis_r,
+                                     ms=0.5):
     """
 
     "picked_locations" should be given as a compartment index
-    
+    we highlight the first picked_locations with a special marker because it will usually be the stimulation point
     """
     # preparing animations params
-    Quant = (Quant0-Quant0.min())/(Quant0.max()-Quant0.min())
-    
+    if segment_condition is None:
+        segment_condition = np.empty(Quant0.shape[0], dtype=bool)+True
+    Quant = (Quant0[segment_condition]-Quant0[segment_condition].min())/(Quant0[segment_condition].max()-Quant0[segment_condition].min())
+        
     # adding inset of time plots and bar legends
     ax2 = add_inset(ax, [0.1,-0.05,.9,.1])
     ax3 = add_inset(ax, [0.83,0.8,.03,.2])
-    build_bar_legend(np.linspace(Quant0.min(), Quant0.max(), 5), ax3, colormap,
+    build_bar_legend(np.linspace(Quant0[segment_condition].min(), Quant0[segment_condition].max(), 5), ax3, colormap,
                      color_discretization=30, label=quant_label)
     
     # picking up locations
     if picked_locations is None:
-        picked_locations = np.concatenate([[0], np.random.randint(1, Quant0.shape[0], 4)])
+        picked_locations = np.concatenate([[0], np.random.randint(1, Quant.shape[0], 4)])
     for pp, p in enumerate(picked_locations):
-        ax2.plot(t, Quant0[p,:], 'k:', lw=1)
-        ax.plot([1e6*SEGMENT_LIST['xcoords'][p]],
-                [1e6*SEGMENT_LIST['ycoords'][p]], 'o',
-                ms=4, color=Color_List[pp])
+        ax2.plot(t, Quant0[segment_condition,:][p,:], 'k:', lw=1)
+        ax.scatter([1e6*SEGMENT_LIST['xcoords'][segment_condition][p]],
+                   [1e6*SEGMENT_LIST['ycoords'][segment_condition][p]], 
+                   s=25+30*(1-np.sign(pp)),
+                   c=list(['k']+Color_List)[pp])
     set_plot(ax2, xlabel=time_label, ylabel=quant_label, num_yticks=2)
 
     LINES = []
-    for pp, p in enumerate(picked_locations):
-        line, = ax2.plot([t[0]], [Quant0[p,0]], 'o', ms=4, color=Color_List[pp])
-        LINES.append(line)
     # plotting each segment
-    line = ax.scatter(1e6*SEGMENT_LIST['xcoords'], 1e6*SEGMENT_LIST['ycoords'],
-                      color=colormap(Quant[:,0]), s=0.2, marker='o')
+    line = ax.scatter(1e6*SEGMENT_LIST['xcoords'][segment_condition], 1e6*SEGMENT_LIST['ycoords'][segment_condition],
+                      color=colormap(Quant[:,0]), s=ms, marker='o')
     LINES.append(line)
+    # then highlighted points
+    for pp, p in enumerate(picked_locations):
+        line, = ax2.plot([t[0]], [Quant0[segment_condition,:][p,0]], 'o',
+                         ms=4+4*(1-np.sign(pp)),
+                         color=list(['k']+Color_List)[pp])
+        LINES.append(line)
     
     # Init only required for blitting to give a clean slate.
     def init():
         return LINES
 
     def animate(i):
-        for j, p in enumerate(picked_locations):
-            LINES[j].set_xdata([t[i]])
-            LINES[j].set_ydata([Quant0[p,i]])
-        LINES[-1].set_color(colormap(Quant[:,i]))  # update the data
+        LINES[0].set_color(colormap(Quant[:,i]))  # update the data
+        for pp, p in enumerate(picked_locations):
+            LINES[pp+1].set_xdata([t[i]])
+            LINES[pp+1].set_ydata([Quant0[segment_condition,:][p,i]])
         return LINES
 
 
