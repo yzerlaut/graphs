fdf3882c931411dfb7ef0811da9bbb797c771ee1
Yann Zerlaut <yann.zerlaut@gmail.com>
2018-11-13 11:37:08 +0100

    adding the bar scales plot

--- annotations.py
+++ annotations.py
@@ -21,28 +21,68 @@ def sci_str(x, rounding=0, remove_0_in_exp=True):
     return y
 
 def bar_scales(ax,
-               Xbar=None, Ybar=None,
-               Xbar_label=None, Ybar_label=None,
+               xlim=None, ylim=None,
+               xbar=0, ybar=0,
+               xbar_label='', ybar_label='',
                location = 'top right',
-               factor=0.95):
+               remove_axis=True,
+               factor=0.98):
 
-    x1, x2 = ax.get_xlim()
-    y1, y2 = ax.get_ylim()
-
-    if location=='top right':
-        x0 = x2-(1-factor)*(x2-x1)
-        y0 = y2-(1-factor)*(y2-y1)
+    if remove_axis:
+        ax.axis('off')
+    
+    if xlim is None:
+        xlim = ax.get_xlim()
+    if ylim is None:
+        ylim = ax.get_xlim()
         
-    if Xbar is not None:
-        ax.plot([x0, x0-Xbar], [y0, y0], 'k-', lw=1)
-        ax.annotate(Xbar_label, (x0-Xbar, y0), fontsize=FONTSIZE)
-    if Ybar is not None:
-        ax.plot([x0, x0], [y0, y0-Ybar], 'k-', lw=1)
-        ax.annotate(Ybar_label, (x0, y0-Ybar), fontsize=FONTSIZE, rotation=90)
+    if location=='top right':
+        x0 = xlim[1]-(1.-factor)*(xlim[1]-xlim[0])
+        y0 = ylim[1]-(1.-factor)*(ylim[1]-ylim[0])
+        ax.plot([x0, x0-xbar], [y0, y0], 'k-', lw=1)
+        ax.plot([x0, x0], [y0, y0-ybar], 'k-', lw=1)
+        ax.annotate(ybar_label, (x0, y0-ybar/2.), fontsize=FONTSIZE, rotation=90)
+        ax.annotate(xbar_label, (x0-xbar, y0), fontsize=FONTSIZE)
+    elif location=='top left':
+        x0 = xlim[0]+(1.-factor)*(xlim[1]-xlim[0])
+        y0 = ylim[1]-(1.-factor)*(ylim[1]-ylim[0])
+        ax.plot([x0, x0+xbar], [y0, y0], 'k-', lw=1)
+        ax.plot([x0, x0], [y0, y0-ybar], 'k-', lw=1)
+        ax.annotate(xbar_label, (x0, y0), fontsize=FONTSIZE)
+        ax.annotate(ybar_label, (x0, y0-ybar/2.), fontsize=FONTSIZE, rotation=90)
+    elif location=='bottom right':
+        x0 = xlim[1]-(1.-factor)*(xlim[1]-xlim[0])
+        y0 = ylim[0]+(1.-factor)*(ylim[1]-ylim[0])
+        ax.plot([x0, x0-xbar], [y0, y0], 'k-', lw=1)
+        ax.plot([x0, x0], [y0, y0+ybar], 'k-', lw=1)
+        ax.annotate(xbar_label, (x0-xbar, y0), fontsize=FONTSIZE)
+        ax.annotate(ybar_label, (x0, y0+ybar/2.), fontsize=FONTSIZE, rotation=90)
+    elif location=='bottom left':
+        x0 = xlim[0]+(1.-factor)*(xlim[1]-xlim[0])
+        y0 = ylim[0]+(1.-factor)*(ylim[1]-ylim[0])
+        ax.plot([x0, x0+xbar], [y0, y0], 'k-', lw=1)
+        ax.plot([x0, x0], [y0, y0+ybar], 'k-', lw=1)
+        ax.annotate(xbar_label, (x0, y0), fontsize=FONTSIZE)
+        ax.annotate(ybar_label, (x0, y0+ybar/2.), fontsize=FONTSIZE, rotation=90)
+    else:
+        x0, y0 = location
+        
 
+    # we reintroduce the data limits
+    ax.set_ylim(ylim)
+    ax.set_xlim(xlim)
     
 
 if __name__=='__main__':
     x = 32.23545345e-5
     print(sci_str(x, rounding=2))
     print(from_pval_to_star(x))
+
+    from my_graph import *
+
+    fig, ax = figure()
+
+    ax.plot(np.random.randn(30), np.random.randn(30), 'o')
+    bar_scales(ax, xbar=2, ybar=2, location='bottom left',
+               ybar_label=r'10$\mu$V', xbar_label='200ms')
+    show()
