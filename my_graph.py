ebd70ace037e67acb8c02c66d65cd5d4106787d9
Yann Zerlaut <yann.zerlaut@gmail.com>
2019-03-14 19:13:09 +0100

    updated the graphs class

--- my_graph.py
+++ my_graph.py
@@ -41,6 +41,7 @@ def update_rcParams(FONTSIZE):
     
 class graphs:
     
+    
     def __init__(self, output_display='manuscript', color='k'):
         """
         accepts styles such as : manuscript, dark_notebook, ggplot_notebook, ...
@@ -67,6 +68,21 @@ class graphs:
             self.override_style = False
         
         update_rcParams(self.FONTSIZE)
+        self.colors = [Blue, Orange, Green, Red, Purple, Brown, Pink, Grey, Kaki, Cyan]
+        self.b, self.o, self.g, self.r, self.purple, self.brown,\
+            self.pink, self.grey, self.kaki, self.cyan = Blue,\
+                            Orange, Green, Red, Purple, Brown, Pink, Grey, Kaki, Cyan
+        self.viridis, self.viridis_r, self.copper, self.copper_r, self.cool, self.jet,\
+            self.PiYG = viridis, viridis_r, copper, copper_r, cool, jet, PiYG
+        self.cmaps = [viridis, viridis_r, copper, copper_r, cool, jet, PiYG]
+        self.blue_to_red = get_linear_colormap(Blue, Red)
+        self.red_to_blue = get_linear_colormap(Red, Blue)
+        self.blue_to_orange = get_linear_colormap(Blue, Orange)
+        self.green_to_red = get_linear_colormap(Green, Red)
+        self.red_to_green = get_linear_colormap(Red, Green)
+        self.green_to_orange = get_linear_colormap(Green, Orange)
+        self.orange_to_green = get_linear_colormap(Orange, Green)
+        
         
     def set_style(self, style='default'):
         plt.style.use(style)
@@ -96,13 +112,86 @@ class graphs:
                                 left, right, bottom, top, wspace, hspace)
             return fig, AX
 
+    def figure_with_bar_legend(self, shift_up=0., shrink=1.):
+        fig, ax = self.figure(figsize=(1.5,1.), right=5.5)
+        acb = add_inset(ax, [1.17, -.08+shift_up, .08, shrink*1.])
+        return fig, ax, acb
+        
+
+    def plot(self,
+             x=None, y=None, sy=None, color='k',
+             X=None, Y=None, sY=None,
+             COLORS=None, colormap=viridis,
+             ax=None,
+             lw=1, alpha_std=0.3, ms=0, m='', ls='-',
+             xlabel='', ylabel='', bar_label='',
+             label=None,
+             LABELS=None,
+             fig_args={},
+             axes_args={},
+             bar_legend_args=None,
+             legend_args=None):
+        
+        """    
+        return fig, ax
+        """
+        # getting or creating the axis
+        if ax is None:
+            fig, ax = self.figure(**fig_args)
+
+        if (y is None) and (Y is None):
+            y = x
+            x = np.arange(len(y))
+
+        if (Y is not None):
+            if (X is None) and (x is not None):
+                X = [x for i in range(len(Y))]
+            elif (X is None):
+                X = [np.arange(len(y)) for y in Y]
+
+            line_plots.multiple_curves(ax, X, Y, sY, COLORS, LABELS,
+                                       alpha_std=alpha_std,
+                                       colormap=colormap,
+                                       lw=lw, ls=ls, m=m, ms=ms)
+        else:
+            line_plots.single_curve(ax, x, y, sy,
+                                    color=color,
+                                    alpha_std=alpha_std,
+                                    lw=lw, label=label, ls=ls, m=m, ms=ms)
+
+        if bar_legend_args is not None:
+            cb = add_inset(ax, **bar_legend_args)
+            build_bar_legend(np.arange(len(LABELS)+1),
+                             cb,
+                             colormap,
+                             label=bar_label,
+                             ticks_labels=LABELS)
+
+        if legend_args is not None:
+            ax.legend(**legend_args)
+
+        if 'xlabel' not in axes_args:
+            axes_args['xlabel'] = xlabel
+        if 'ylabel' not in axes_args:
+            axes_args['ylabel'] = ylabel
+        self.set_plot(ax, **axes_args)
+
+        return ax
+        
+    ################################################
+    ###### Annotate function #######################
+    ################################################
     def annotate(self, stuff, s,
                  xy, xycoords='axes fraction', bold=False, italic=False, rotation=0,
-                 fontsize=None, color=None, ha='left', va='bottom', weight='normal', style='normal'):
+                 fontsize=None, size=None, color=None, ha='left', va='bottom', weight='normal', style='normal'):
         """
         """
         if fontsize is None:
             fontsize=self.FONTSIZE
+        if size=='small':
+            fontsize=self.FONTSIZE-1
+        elif size=='x-small':
+            fontsize=self.FONTSIZE-2
         if color is None:
             color=self.default_color
         if bold and (weight=='normal'):
@@ -113,11 +202,11 @@ class graphs:
         if type(stuff)==mpl.figure.Figure: # if figure, no choice, if figure relative coordinates
             plt.annotate(s, xy, xycoords='figure fraction',
                          weight=weight, fontsize=fontsize, style=style,
-                         color=color, rotation=rotation)
+                         color=color, rotation=rotation, ha=ha, va=va)
         else:
             stuff.annotate(s, xy, xycoords=xycoords,
                            weight=weight, fontsize=fontsize, style=style,
-                           color=color, rotation=rotation)
+                           color=color, rotation=rotation, ha=ha, va=va)
 
     def top_left_letter(self, ax, s, xy=(-0.3,1.02), bold=True, fontsize=None):
         if fontsize is None:
@@ -174,28 +263,28 @@ class graphs:
             input('Hit Enter To Close')
             plt.close()
 
-    ###########################################################
-    ###### Now a set of predefined functions
-    ###########################################################
+###########################################################
+###### Now a set of predefined functions
+###########################################################
     
     
 ###########################################################
 ###### a versatile line plot function
 ###########################################################
 
-def plot(x=None, y=None, sy=None,
-         color='k',
-         X=None, Y=None, sY=None,
-         COLORS=None, colormap=viridis,
-         ax=None,
-         lw=1, alpha_std=0.3, ms=0, m='', ls='-',
-         xlabel='', ylabel='', bar_label='',
-         label=None,
-         LABELS=None,
-         fig_args={},
-         axes_args={},
-         bar_legend_args=None,
-         legend_args=None):
+def plot_function(x=None, y=None, sy=None,
+                  color='k',
+                  X=None, Y=None, sY=None,
+                  COLORS=None, colormap=viridis,
+                  ax=None,
+                  lw=1, alpha_std=0.3, ms=0, m='', ls='-',
+                  xlabel='', ylabel='', bar_label='',
+                  label=None,
+                  LABELS=None,
+                  fig_args={},
+                  axes_args={},
+                  bar_legend_args=None,
+                  legend_args=None):
 
     """    
     return fig, ax
