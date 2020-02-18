b6d5e1d781002d4c17d23ef86721dd8a6fbe3af9
Yann Zerlaut <yann.zerlaut@gmail.com>
2020-02-18 11:06:58 +0100

    adding a setup.py file

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='graphs',
+    version='0.1',
+    description='A layer on top of `matplotlib` to achieve flexible & high-standard data visualization across different mediums',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/yzerlaut/graphs',
+    author='Yann Zerlaut',
+    author_email='yann.zerlaut@cnrs.fr',
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    keywords='data visualization matplotlib',
+    packages=find_packages(),
+    install_requires=[
+        "matplotlib",
+        "svgutils",
+        "numpy",
+        "scipy"
+    ]
+)
